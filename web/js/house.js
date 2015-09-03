137bc9df5b082a9cdd242b0a0e29fb05bc371a75
Jordi Tost <tost.jordi@gmail.com>
2015-09-03 18:58:20 +0200

    Add popups when clicking on house view elements

--- web/js/house.js
+++ web/js/house.js
@@ -3,7 +3,7 @@
 // SVG Vars
 //////////////
 
-var s;
+var svg;
 
 ////////////
 // Colors
@@ -37,14 +37,27 @@ window.onload = function () {
 	// Snap.load("images/house/3rd_Floor_WG_Active.svg", function (f) {
 	Snap.load("images/house/optimised.svg", function (f) {
 
-		var g 		= f.select("g"),
-			floor2  = f.select("#apartment_x5F_Inactive"),
-			boy1 	= f.select("#Boy_x5F_1"),
-			boy2 	= f.select("#Boy_x5F_2"),
-			girl 	= f.select("#Girl"),
-			lamp 	= f.select("#Lamp");
+		var g 			= f.select("g"),
+			floor2  	= f.select("#apartment_x5F_Inactive"),
 
-			//top = s.g();
+			// Solutions
+			boy1 	 	= f.select("#Boy_x5F_1"),
+			boy2 		= f.select("#Boy_x5F_2"),
+			girl 		= f.select("#Girl"),
+			bottle   	= f.select("#Bottle"),
+			
+			// Problems
+			lamp 		= f.select("#Lamp"),
+			cabinetOp 	= f.select("#Cabinet_x5F_Open"),
+			cabinetCl 	= f.select("#Cabinet_x5F_Closed"),
+			counterOp 	= f.select("#Counter_x5F_Open"),
+			counterCl 	= f.select("#Counter_x5F_Closed"),
+			laptop 		= f.select("#Laptop"),
+			wc 			= f.select("#WC"),
+			sink 		= f.select("#Sink");
+
+		// console.log(g.getBBox().x);
+		// console.log(g.getBBox().y);
 
 		//g.drag();
 
@@ -64,57 +77,99 @@ window.onload = function () {
 			animateTest();
 		});
 
-		boy1.click(function(evt) {
-			activateGraphic(this);
-			evt.stopPropagation();
-		});
+		// Solutions
+		boy1.click(onElementClicked);
+		boy2.click(onElementClicked);
+		girl.click(onElementClicked);
+		bottle.click(onElementClicked);
 
-		boy2.click(function(evt) {
-			activateGraphic(this);
-			evt.stopPropagation();
-		});
-
-		girl.click(function(evt) {
-			activateGraphic(this);
-			evt.stopPropagation();
-		});
+		// Problems
+		lamp.click(onElementClicked);
+		cabinetOp.click(onElementClicked);
+		counterOp.click(onElementClicked);
+		laptop.click(onElementClicked);
+		wc.click(onElementClicked);
+		sink.click(onElementClicked);
 
 		// Click on the whole graphic
 		$(document).click(function(e) {
 			$('.active-graphic').removeAttr("class");
+			var $popupPane = $('.leaflet-popup-pane').remove();
 		});
 
-		// Add loaded graphic (g) to svg element (s)
+		// Add loaded graphic (g) to svg element (svg)
 		//top.add(gr);
 		svg.append(g);
 	});
 };
 
+/////////////////////////
+// Click Event Handler
+/////////////////////////
+
+var onElementClicked = function(evt) {
+
+	// Check http://snapsvg.io/docs/#Element.getBBox()
+	//console.log("x: " + this.getBBox().x + ", y: " + this.getBBox().y);
+	//console.log("Click coordinates - x: " + evt.x + ", y: " + evt.y);
+
+	// var c = svg.circle(evt.x, evt.y, 10);
+	// c.attr({
+	//     fill: '#f00'
+	// });
+
+	activateGraphic(this);
+	showPopup(this, evt);
+	evt.stopPropagation();
+}
+
+function showPopup(graphic, evt) {
+
+	var $this = $(graphic.node),
+		bbox  = $this[0].getBBox();
+
+	// var c = svg.circle(bbox.x, bbox.y, 10);
+	// c.attr({
+	//     fill: '#f00'
+	// });
+
+	//console.log("Element coordinates - x: " + bbox.x + ", y: " + bbox.y);
+
+	// Add popup in this position
+	var x = evt.x - 185, // bbox.x,
+		y = evt.y - 50; // bbox.y;
+
+	// Remove old popups
+	var $popupPane = $('.leaflet-popup-pane').remove();
+
+	// Create new pane
+	$popupPane = $('<div class="leaflet-popup-pane"><div class="leaflet-popup  leaflet-zoom-animated" style="opacity: 1; transform: translate3d('+x+'px, '+y+'px, 0px); bottom: 27px; left: -82px;"><a class="leaflet-popup-close-button" href="#close">×</a><div class="leaflet-popup-content-wrapper"><div class="leaflet-popup-content" style="width: 133px;"><h4>House</h4><p>This is our house!</p></div></div><div class="leaflet-popup-tip-container"><div class="leaflet-popup-tip"></div></div></div></div>');
+		//$popup = $popupPane.find('leaflet-popup').css({left: evt.x+"px", top: evt.y+"px"});
+
+	$('#house-view').append($popupPane);
+}
+
 ///////////////////
 // SVG Functions
 ///////////////////
 
-function activateGraphic(graphic, activate) {
+function activateGraphic(graphic) {
 
-	if (typeof activate === 'undefined') activate = true;
+	// Deactivate other active elements
+	$('.active-graphic').removeAttr("class");
 
-	if (activate) {
+	// "node" selects the DOM element. 
+	// then we can wrap it into a jQuery element ($)
+	var $this = $(graphic.node);
+	$this.attr("class", "active-graphic");
 
-		// Deactivate other active elements
-		$('.active-graphic').removeAttr("class");
-		
-		// "node" selects the DOM element. 
-		// then we can wrap it into a jQuery element ($)
-		var $this = $(graphic.node);
-		$this.attr("class", "active-graphic");
+	//console.log($this[0].getBBox());
+	// console.log("$this - x: " + $this.offset.x + ", y: " + $this.offset.y);
 
-		//changeColor(graphic, selectedStroke, selectedFill, selectedFillOpacity);
-		// var trans = new Snap.Matrix();
-		// trans.translate(0, -200);
-		// graphic.select('g').animate({ transform: transform.toTransformString() }, 500);
-	} else {
-		//changeColor(graphic, solutionsStroke, solutionsFill, solutionsFillOpacity);
-	}
+	//changeColor(graphic, selectedStroke, selectedFill, selectedFillOpacity);
+	// var trans = new Snap.Matrix();
+	// trans.translate(0, -200);
+	// graphic.select('g').animate({ transform: transform.toTransformString() }, 500);
 }
 
 //////////////////////
